34a9b5f76720d47e17e85cb865e6168969fb79be
Luis Weich <luis.gabriel.weich@gmail.com>
2025-09-12 17:18:40 +0200

    OpenTelemetryService update

--- ludrive-server/src/main/java/app/ludrive/server/otel/OpenTelemetryService.java
+++ ludrive-server/src/main/java/app/ludrive/server/otel/OpenTelemetryService.java
@@ -1,8 +1,5 @@
 package app.ludrive.server.otel;
 
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-
 import app.ludrive.core.domain.management.auth.AuthIdentity;
 import app.ludrive.core.service.telemetry.TelemetryService;
 
@@ -10,14 +7,12 @@ import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.metrics.LongCounter;
 import io.opentelemetry.api.metrics.Meter;
 
-@ApplicationScoped
 public class OpenTelemetryService implements TelemetryService {
 
     public static final String SUCCESSFUL_REQUESTS = "successful-requests";
 
     protected final LongCounter successCounter;
 
-    @Inject
     public OpenTelemetryService(Meter meter) {
         this.successCounter = meter.counterBuilder(SUCCESSFUL_REQUESTS).build();
     }
