9c397fd9cfeeb9af010058a5404d1f9203def45f
Luis Weich <luis.gabriel.weich@gmail.com>
2025-09-12 15:53:16 +0200

    Events use singletonList

--- ludrive-core/src/main/java/app/ludrive/core/service/event/Events.java
+++ ludrive-core/src/main/java/app/ludrive/core/service/event/Events.java
@@ -16,105 +16,105 @@ public final class Events {
 
     public record DriveUserCreatedProps(AuthIdentity identity, Collection<UUID> driveUserIds) implements EventProps {
         public DriveUserCreatedProps(AuthIdentity identity, UUID driveUserId) {
-            this(identity, Collections.singleton(driveUserId));
+            this(identity, Collections.singletonList(driveUserId));
         }
     }
 
     public record DriveUserReadProps(AuthIdentity identity, Collection<UUID> driveUserIds) implements EventProps {
         public DriveUserReadProps(AuthIdentity identity, UUID driveUserId) {
-            this(identity, Collections.singleton(driveUserId));
+            this(identity, Collections.singletonList(driveUserId));
         }
     }
 
     public record DriveUserUpdatedProps(AuthIdentity identity, Collection<UUID> driveUserIds) implements EventProps {
         public DriveUserUpdatedProps(AuthIdentity identity, UUID driveUserId) {
-            this(identity, Collections.singleton(driveUserId));
+            this(identity, Collections.singletonList(driveUserId));
         }
     }
 
     public record DriveUserDeletedProps(AuthIdentity identity, Collection<UUID> driveUserIds) implements EventProps {
         public DriveUserDeletedProps(AuthIdentity identity, UUID driveUserId) {
-            this(identity, Collections.singleton(driveUserId));
+            this(identity, Collections.singletonList(driveUserId));
         }
     }
 
     public record EntryCreatedProps(AuthIdentity identity, Collection<UUID> entryIds) implements EventProps {
         public EntryCreatedProps(AuthIdentity identity, UUID entryId) {
-            this(identity, Collections.singleton(entryId));
+            this(identity, Collections.singletonList(entryId));
         }
     }
 
     public record EntryReadProps(AuthIdentity identity, Collection<UUID> entryIds) implements EventProps {
         public EntryReadProps(AuthIdentity identity, UUID entryId) {
-            this(identity, Collections.singleton(entryId));
+            this(identity, Collections.singletonList(entryId));
         }
     }
 
     public record EntryUpdatedProps(AuthIdentity identity, Collection<UUID> entryIds) implements EventProps {
         public EntryUpdatedProps(AuthIdentity identity, UUID entryId) {
-            this(identity, Collections.singleton(entryId));
+            this(identity, Collections.singletonList(entryId));
         }
     }
 
     public record EntryDeletedProps(AuthIdentity identity, Collection<UUID> entryIds) implements EventProps {
         public EntryDeletedProps(AuthIdentity identity, UUID entryId) {
-            this(identity, Collections.singleton(entryId));
+            this(identity, Collections.singletonList(entryId));
         }
     }
 
     public record DirectoryCreatedProps(AuthIdentity identity, UUID entryId, Collection<EntryItemId> directoryIds)
             implements EventProps {
         public DirectoryCreatedProps(AuthIdentity identity, UUID entryId, EntryItemId directoryId) {
-            this(identity, entryId, Collections.singleton(directoryId));
+            this(identity, entryId, Collections.singletonList(directoryId));
         }
     }
 
     public record DirectoryReadProps(AuthIdentity identity, UUID entryId, Collection<EntryItemId> directoryIds)
             implements EventProps {
         public DirectoryReadProps(AuthIdentity identity, UUID entryId, EntryItemId directoryId) {
-            this(identity, entryId, Collections.singleton(directoryId));
+            this(identity, entryId, Collections.singletonList(directoryId));
         }
     }
 
     public record DirectoryUpdatedProps(AuthIdentity identity, UUID entryId, Collection<EntryItemId> directoryIds)
             implements EventProps {
         public DirectoryUpdatedProps(AuthIdentity identity, UUID entryId, EntryItemId directoryId) {
-            this(identity, entryId, Collections.singleton(directoryId));
+            this(identity, entryId, Collections.singletonList(directoryId));
         }
     }
 
     public record DirectoryDeletedProps(AuthIdentity identity, UUID entryId, Collection<EntryItemId> directoryIds)
             implements EventProps {
         public DirectoryDeletedProps(AuthIdentity identity, UUID entryId, EntryItemId directoryId) {
-            this(identity, entryId, Collections.singleton(directoryId));
+            this(identity, entryId, Collections.singletonList(directoryId));
         }
     }
 
     public record FileCreatedProps(AuthIdentity identity, UUID entryId, Collection<EntryItemId> fileIds)
             implements EventProps {
         public FileCreatedProps(AuthIdentity identity, UUID entryId, EntryItemId fileId) {
-            this(identity, entryId, Collections.singleton(fileId));
+            this(identity, entryId, Collections.singletonList(fileId));
         }
     }
 
     public record FileReadProps(AuthIdentity identity, UUID entryId, Collection<EntryItemId> fileIds)
             implements EventProps {
         public FileReadProps(AuthIdentity identity, UUID entryId, EntryItemId fileId) {
-            this(identity, entryId, Collections.singleton(fileId));
+            this(identity, entryId, Collections.singletonList(fileId));
         }
     }
 
     public record FileUpdatedProps(AuthIdentity identity, UUID entryId, Collection<EntryItemId> fileIds)
             implements EventProps {
         public FileUpdatedProps(AuthIdentity identity, UUID entryId, EntryItemId fileId) {
-            this(identity, entryId, Collections.singleton(fileId));
+            this(identity, entryId, Collections.singletonList(fileId));
         }
     }
 
     public record FileDeletedProps(AuthIdentity identity, UUID entryId, Collection<EntryItemId> fileIds)
             implements EventProps {
         public FileDeletedProps(AuthIdentity identity, UUID entryId, EntryItemId fileId) {
-            this(identity, entryId, Collections.singleton(fileId));
+            this(identity, entryId, Collections.singletonList(fileId));
         }
     }
 
