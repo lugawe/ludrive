750c41dd2b4d26e477b79ef2b5f022f31bf96fc8
Luis Weich <luis.gabriel.weich@gmail.com>
2025-09-17 21:39:57 +0200

    add ContextService

---
+++ ludrive-core/src/main/java/app/ludrive/core/service/context/ContextService.java
@@ -0,0 +1,20 @@
+package app.ludrive.core.service.context;
+
+import java.util.UUID;
+
+import app.ludrive.core.domain.management.Entry;
+import app.ludrive.core.domain.management.auth.AuthIdentity;
+import app.ludrive.core.domain.management.auth.AuthIdentityEntryKey;
+
+public interface ContextService {
+
+    AuthIdentity getAuthIdentity();
+
+    Entry getEntry();
+
+    default AuthIdentityEntryKey getKey() {
+        UUID authIdentityId = getAuthIdentity().getId();
+        UUID entryId = getEntry().getId();
+        return new AuthIdentityEntryKey(authIdentityId, entryId);
+    }
+}
