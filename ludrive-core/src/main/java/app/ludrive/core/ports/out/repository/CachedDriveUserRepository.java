22ff2a40f05c140a71a73ef527748399adcb1c20
Luis Weich <luis.gabriel.weich@gmail.com>
2025-09-17 23:11:19 +0200

    fix

--- ludrive-core/src/main/java/app/ludrive/core/ports/out/repository/CachedDriveUserRepository.java
+++ ludrive-core/src/main/java/app/ludrive/core/ports/out/repository/CachedDriveUserRepository.java
@@ -36,12 +36,6 @@ public class CachedDriveUserRepository extends AbstractCachedRepository<DriveUse
     }
 
     @Override
-    public void onDriveUserCreated(Events.DriveUserCreatedProps props) {}
-
-    @Override
-    public void onDriveUserRead(Events.DriveUserReadProps props) {}
-
-    @Override
     public void onDriveUserUpdated(Events.DriveUserUpdatedProps props) {
         Collection<UUID> driveUserIds = props.driveUserIds();
         evict(driveUserIds);
@@ -52,40 +46,4 @@ public class CachedDriveUserRepository extends AbstractCachedRepository<DriveUse
         Collection<UUID> driveUserIds = props.driveUserIds();
         evict(driveUserIds);
     }
-
-    @Override
-    public void onEntryCreated(Events.EntryCreatedProps props) {}
-
-    @Override
-    public void onEntryRead(Events.EntryReadProps props) {}
-
-    @Override
-    public void onEntryUpdated(Events.EntryUpdatedProps props) {}
-
-    @Override
-    public void onEntryDeleted(Events.EntryDeletedProps props) {}
-
-    @Override
-    public void onDirectoryCreated(Events.DirectoryCreatedProps props) {}
-
-    @Override
-    public void onDirectoryRead(Events.DirectoryReadProps props) {}
-
-    @Override
-    public void onDirectoryUpdated(Events.DirectoryUpdatedProps props) {}
-
-    @Override
-    public void onDirectoryDeleted(Events.DirectoryDeletedProps props) {}
-
-    @Override
-    public void onFileCreated(Events.FileCreatedProps props) {}
-
-    @Override
-    public void onFileRead(Events.FileReadProps props) {}
-
-    @Override
-    public void onFileUpdated(Events.FileUpdatedProps props) {}
-
-    @Override
-    public void onFileDeleted(Events.FileDeletedProps props) {}
 }
