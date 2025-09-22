4a0dfce478d8b2e9b3159a9217754360ad3f9666
Luis Weich <luis.gabriel.weich@gmail.com>
2025-09-22 20:41:54 +0200

    AuthIdentityEntryKey get and from key string

--- ludrive-core/src/main/java/app/ludrive/core/service/context/AuthIdentityEntryKey.java
+++ ludrive-core/src/main/java/app/ludrive/core/service/context/AuthIdentityEntryKey.java
@@ -3,4 +3,28 @@ package app.ludrive.core.service.context;
 import java.io.Serializable;
 import java.util.UUID;
 
-public record AuthIdentityEntryKey(UUID authIdentityId, UUID entryId) implements Serializable {}
+public record AuthIdentityEntryKey(UUID authIdentityId, UUID entryId) implements Serializable {
+
+    private static final String DELIMITER = ":";
+
+    public String getKeyString() {
+        return authIdentityId + DELIMITER + entryId;
+    }
+
+    public static AuthIdentityEntryKey fromKeyString(String keyString) {
+
+        if (keyString == null) {
+            throw new NullPointerException("keyString");
+        }
+
+        String[] parts = keyString.split(DELIMITER);
+        if (parts.length != 2) {
+            throw new IllegalArgumentException("keyString");
+        }
+
+        UUID authIdentityId = UUID.fromString(parts[0]);
+        UUID entryId = UUID.fromString(parts[1]);
+
+        return new AuthIdentityEntryKey(authIdentityId, entryId);
+    }
+}
