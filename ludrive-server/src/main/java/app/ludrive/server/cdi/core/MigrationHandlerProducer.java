365e2ffbdddde9d1bb5151b7c3aad43ef203988a
Luis Weich <luis.gabriel.weich@gmail.com>
2025-09-30 18:37:02 +0200

    fix

--- ludrive-server/src/main/java/app/ludrive/server/cdi/core/MigrationHandlerProducer.java
+++ ludrive-server/src/main/java/app/ludrive/server/cdi/core/MigrationHandlerProducer.java
@@ -1,12 +1,13 @@
 package app.ludrive.server.cdi.core;
 
-import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
 
 import app.ludrive.core.ports.out.migration.MigrationHandler;
 import app.ludrive.core.ports.out.migration.NoMigrationHandler;
 
-@RequestScoped
+// TODO should be request scoped
+@ApplicationScoped
 public class MigrationHandlerProducer {
 
     public MigrationHandlerProducer() {}
