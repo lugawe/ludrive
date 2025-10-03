f7301f16cf7d497fb5cbd221e6f6b7255721edd6
Luis Weich <luis.gabriel.weich@gmail.com>
2025-10-03 21:51:48 +0200

    create vfs2 services

--- ludrive-persistence-vfs/src/main/java/app/ludrive/adapters/out/persistence/vfs/fs/VFS2Service.java
+++ ludrive-persistence-vfs/src/main/java/app/ludrive/adapters/out/persistence/vfs/fs/VFS2Service.java
@@ -1,30 +1,17 @@
 package app.ludrive.adapters.out.persistence.vfs.fs;
 
-import java.nio.channels.Channel;
-
-import jakarta.enterprise.context.RequestScoped;
-
 import app.ludrive.core.domain.vfs.Directory;
-import app.ludrive.core.domain.vfs.File;
-import app.ludrive.core.service.validation.Validator;
+import app.ludrive.core.domain.vfs.FileContent;
 import app.ludrive.core.service.vfs.VirtualFSService;
 
-@RequestScoped
+// TODO
 public class VFS2Service implements VirtualFSService {
 
-    private final Validator validator;
-
-    public VFS2Service(Validator validator) {
-        this.validator = validator;
-    }
+    public VFS2Service() {}
 
     @Override
-    public void createDirectory(Directory directory) {
-        validator.validateDirectory(directory);
-    }
+    public void createDirectory(Directory directory) {}
 
     @Override
-    public void createFile(File file, Channel fileContent) {
-        validator.validateFile(file, fileContent);
-    }
+    public void createFile(FileContent fileContent) {}
 }
