cecefe693da91be56f7cf3e717c4b4e5f221923e
Luis Weich <luis.gabriel.weich@gmail.com>
2025-10-04 19:56:27 +0200

    fix

--- ludrive-persistence-vfs/src/main/java/app/ludrive/adapters/out/persistence/vfs/fs/VFS2Service.java
+++ ludrive-persistence-vfs/src/main/java/app/ludrive/adapters/out/persistence/vfs/fs/VFS2Service.java
@@ -1,7 +1,9 @@
 package app.ludrive.adapters.out.persistence.vfs.fs;
 
 import app.ludrive.core.domain.vfs.Directory;
+import app.ludrive.core.domain.vfs.File;
 import app.ludrive.core.domain.vfs.FileContent;
+import app.ludrive.core.exception.VFSException;
 import app.ludrive.core.service.vfs.VirtualFSService;
 
 // TODO
@@ -10,8 +12,28 @@ public class VFS2Service implements VirtualFSService {
     public VFS2Service() {}
 
     @Override
-    public void createDirectory(Directory directory) {}
+    public void createDirectory(Directory directory) throws VFSException {}
 
     @Override
-    public void createFile(FileContent fileContent) {}
+    public void updateDirectory(String path, Directory directory) throws VFSException {}
+
+    @Override
+    public void deleteDirectory(String path) throws VFSException {}
+
+    @Override
+    public void createFile(FileContent fileContent) throws VFSException {}
+
+    @Override
+    public void updateFile(String path, File file) throws VFSException {}
+
+    @Override
+    public void updateFileContent(String path, FileContent fileContent) throws VFSException {}
+
+    @Override
+    public void deleteFile(String path) throws VFSException {}
+
+    @Override
+    public FileContent getFileContent(File file) throws VFSException {
+        return null;
+    }
 }
