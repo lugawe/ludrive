63ba013bb4e7563c65ffd439a6e531aacf2b38f0
Luis Weich <luis.gabriel.weich@gmail.com>
2025-10-04 19:12:33 +0200

    MemoryVFSTree checks

--- ludrive-persistence-vfs/src/main/java/app/ludrive/adapters/out/persistence/vfs/fs/tree/MemoryVFSTree.java
+++ ludrive-persistence-vfs/src/main/java/app/ludrive/adapters/out/persistence/vfs/fs/tree/MemoryVFSTree.java
@@ -18,21 +18,49 @@ public class MemoryVFSTree implements VFSTree {
 
     @Override
     public void put(String path, EntryItem entryItem) {
+
+        if (path == null) {
+            throw new NullPointerException("path");
+        }
+        if (entryItem == null) {
+            throw new NullPointerException("entryItem");
+        }
+
+        if (!path.equals(entryItem.getPath())) {
+            throw new IllegalArgumentException("path and entry item path must match");
+        }
+
         items.put(path, entryItem);
     }
 
     @Override
     public EntryItem get(String path) {
+
+        if (path == null) {
+            throw new NullPointerException("path");
+        }
+
         return items.get(path);
     }
 
+    // TODO does do stupid things, gets fixed in future
     @Override
     public Stream<? extends EntryItem> list(String path) {
+
+        if (path == null) {
+            throw new NullPointerException("path");
+        }
+
         return items.values().stream().filter(e -> e.getPath().startsWith(path));
     }
 
     @Override
     public void remove(String path) {
+
+        if (path == null) {
+            throw new NullPointerException("path");
+        }
+
         items.remove(path);
     }
 }
