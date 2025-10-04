58e41119371b142108529668a62308f88ba57d41
Luis Weich <luis.gabriel.weich@gmail.com>
2025-10-04 19:31:26 +0200

    remove RequestScoped from MemoryVFSTree

--- ludrive-persistence-vfs/src/main/java/app/ludrive/adapters/out/persistence/vfs/fs/tree/MemoryVFSTree.java
+++ ludrive-persistence-vfs/src/main/java/app/ludrive/adapters/out/persistence/vfs/fs/tree/MemoryVFSTree.java
@@ -4,11 +4,8 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
 
-import jakarta.enterprise.context.RequestScoped;
-
 import app.ludrive.core.domain.vfs.EntryItem;
 
-@RequestScoped
 public class MemoryVFSTree implements VFSTree {
 
     // TODO better data structure
