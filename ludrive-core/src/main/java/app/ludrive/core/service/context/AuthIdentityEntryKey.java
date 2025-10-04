a8288f91b41f9c04c4fa2793f275cdf49e0b2e21
Luis Weich <luis.gabriel.weich@gmail.com>
2025-10-05 01:29:10 +0200

    AuthIdentityEntryKey null safe constructor

--- ludrive-core/src/main/java/app/ludrive/core/service/context/AuthIdentityEntryKey.java
+++ ludrive-core/src/main/java/app/ludrive/core/service/context/AuthIdentityEntryKey.java
@@ -1,11 +1,17 @@
 package app.ludrive.core.service.context;
 
 import java.io.Serializable;
+import java.util.Objects;
 import java.util.UUID;
 
 public record AuthIdentityEntryKey(UUID authIdentityId, UUID entryId) implements Serializable {
 
-    private static final String DELIMITER = "::";
+    private static final String DELIMITER = ":";
+
+    public AuthIdentityEntryKey(UUID authIdentityId, UUID entryId) {
+        this.authIdentityId = Objects.requireNonNull(authIdentityId);
+        this.entryId = Objects.requireNonNull(entryId);
+    }
 
     public String getKeyString() {
         return authIdentityId + DELIMITER + entryId;
