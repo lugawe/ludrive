2adba4afb13b5c8ecbcf1a8cb1ff97e9b121c350
Luis Weich <luis.gabriel.weich@gmail.com>
2025-10-04 19:16:40 +0200

    EntryItemId null check

--- ludrive-core/src/main/java/app/ludrive/core/domain/vfs/EntryItemId.java
+++ ludrive-core/src/main/java/app/ludrive/core/domain/vfs/EntryItemId.java
@@ -1,6 +1,13 @@
 package app.ludrive.core.domain.vfs;
 
 import java.io.Serializable;
+import java.util.Objects;
 import java.util.UUID;
 
-public record EntryItemId(UUID entryId, String path) implements Serializable {}
+public record EntryItemId(UUID entryId, String path) implements Serializable {
+
+    public EntryItemId(UUID entryId, String path) {
+        this.entryId = Objects.requireNonNull(entryId, "entryId");
+        this.path = Objects.requireNonNull(path, "path");
+    }
+}
