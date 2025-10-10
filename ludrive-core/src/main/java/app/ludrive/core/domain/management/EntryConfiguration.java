4bea804551156d962a8010de531eed0efae6ead3
Luis Weich <luis.gabriel.weich@gmail.com>
2025-10-11 00:32:32 +0200

    update EntryConfiguration

--- ludrive-core/src/main/java/app/ludrive/core/domain/management/EntryConfiguration.java
+++ ludrive-core/src/main/java/app/ludrive/core/domain/management/EntryConfiguration.java
@@ -2,11 +2,8 @@ package app.ludrive.core.domain.management;
 
 import java.io.Serializable;
 import java.util.Map;
-import java.util.UUID;
 
-import app.ludrive.core.domain.Identifiable;
-
-public final class EntryConfiguration implements Identifiable, Serializable {
+public final class EntryConfiguration implements Serializable {
 
     public enum Type {
         MEMORY,
@@ -15,33 +12,13 @@ public final class EntryConfiguration implements Identifiable, Serializable {
         S3
     }
 
-    private UUID id;
-    private String name;
-
     private Type type;
+
     private String rootPath;
 
     private Map<String, String> credentials;
 
     public EntryConfiguration() {}
-
-    @Override
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public Type getType() {
         return type;
