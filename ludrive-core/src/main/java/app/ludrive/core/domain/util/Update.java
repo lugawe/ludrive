54698620143763da2149ec3f129113d96dddb101
Luis Weich <luis.gabriel.weich@gmail.com>
2025-10-15 01:22:58 +0200

    value and update is serializable

--- ludrive-core/src/main/java/app/ludrive/core/domain/util/Update.java
+++ ludrive-core/src/main/java/app/ludrive/core/domain/util/Update.java
@@ -1,5 +1,7 @@
 package app.ludrive.core.domain.util;
 
+import java.io.Serializable;
+
 import app.ludrive.core.domain.Identifiable;
 
-public record Update<T extends Identifiable>(T oldValue, T newValue) {}
+public record Update<T extends Identifiable>(T oldValue, T newValue) implements Serializable {}
--- ludrive-core/src/main/java/app/ludrive/core/domain/util/Value.java
+++ ludrive-core/src/main/java/app/ludrive/core/domain/util/Value.java
@@ -1,5 +1,7 @@
 package app.ludrive.core.domain.util;
 
+import java.io.Serializable;
+
 import app.ludrive.core.domain.Identifiable;
 
-public record Value<T extends Identifiable>(T value) {}
+public record Value<T extends Identifiable>(T value) implements Serializable {}
