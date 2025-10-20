cc796882ab5fe6c01567d9645ac2e75edf7475da
Luis Weich <luis.gabriel.weich@gmail.com>
2025-10-20 18:56:19 +0200

    update caches

--- ludrive-core/src/main/java/app/ludrive/core/service/cache/Cache.java
+++ ludrive-core/src/main/java/app/ludrive/core/service/cache/Cache.java
@@ -2,18 +2,23 @@ package app.ludrive.core.service.cache;
 
 import java.util.Collection;
 import java.util.Optional;
+import java.util.SequencedCollection;
 
-public interface Cache<T, ID> {
+import app.ludrive.core.domain.Identifiable;
+
+public interface Cache<T extends Identifiable, ID> {
 
     void setValue(ID id, T value);
 
     Optional<T> getValue(ID id);
 
+    SequencedCollection<T> getValues(Collection<ID> ids);
+
     boolean containsValue(ID id);
 
     void evict(ID id);
 
-    void evict(Collection<? extends ID> ids);
+    void evict(Collection<ID> ids);
 
     void evictAll();
 }
---
+++ ludrive-core/src/main/java/app/ludrive/core/service/cache/MemoryCache.java
@@ -0,0 +1,80 @@
+package app.ludrive.core.service.cache;
+
+import java.util.*;
+
+import app.ludrive.core.domain.Identifiable;
+import app.ludrive.core.exception.CacheException;
+
+public class MemoryCache<T extends Identifiable, ID> implements Cache<T, ID> {
+
+    protected final Map<ID, T> cache = Collections.synchronizedMap(new LinkedHashMap<>());
+
+    public MemoryCache() {}
+
+    @Override
+    public void setValue(ID id, T value) {
+
+        if (id == null) {
+            throw new CacheException("Id cannot be null");
+        }
+
+        cache.put(id, value);
+    }
+
+    @Override
+    public Optional<T> getValue(ID id) {
+
+        if (id == null) {
+            throw new CacheException("Id cannot be null");
+        }
+
+        T value = cache.get(id);
+
+        return Optional.ofNullable(value);
+    }
+
+    @Override
+    public SequencedCollection<T> getValues(Collection<ID> ids) {
+
+        if (ids == null) {
+            throw new CacheException("Ids cannot be null");
+        }
+
+        return ids.stream().map(cache::get).filter(Objects::nonNull).toList();
+    }
+
+    @Override
+    public boolean containsValue(ID id) {
+
+        if (id == null) {
+            throw new CacheException("Id cannot be null");
+        }
+
+        return cache.containsKey(id);
+    }
+
+    @Override
+    public void evict(ID id) {
+
+        if (id == null) {
+            throw new CacheException("Id cannot be null");
+        }
+
+        cache.remove(id);
+    }
+
+    @Override
+    public void evict(Collection<ID> ids) {
+
+        if (ids == null) {
+            throw new CacheException("Ids cannot be null");
+        }
+
+        cache.keySet().removeAll(ids);
+    }
+
+    @Override
+    public void evictAll() {
+        cache.clear();
+    }
+}
